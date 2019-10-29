ff16cb564033ac71e262d4eebc8c061014cdeb7d
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-10-28 21:16:05 -0500

    crear botones

--- src/application/ControllerWindow.java
+++ src/application/ControllerWindow.java
@@ -5,10 +5,14 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.GridPane;
+import modelo.Buscaminas;
 
 public class ControllerWindow implements Initializable {
 	
+	private Buscaminas buscamina;
 	@FXML
 	private ChoiceBox<String> choiceBox;
 
@@ -25,7 +29,22 @@ public class ControllerWindow implements Initializable {
 	}
 	
 	public void charge(ActionEvent  a) {
-		
+		GridPane gPane = new GridPane();
+		Button button = new Button();
+		if() {
+			for(int i =0; i<= Buscaminas.FILAS_PRINCIPIANTE; i++) {
+				for(int j = 0; j <= Buscaminas.COLUMNAS_PRINCIPIANTE; j++) {
+					
+				}
+			}
+			
+		}
+		if() {
+			
+		}
+		if() {
+			
+		}
 	}
 
 }
