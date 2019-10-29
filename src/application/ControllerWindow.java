1ff0363eb41a3e0871c38c13383483ad610040e6
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-10-28 19:08:42 -0500

    se agrega ventana

--- src/application/ControllerWindow.java
+++ src/application/ControllerWindow.java
@@ -2,14 +2,29 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
 public class ControllerWindow implements Initializable {
+	
+	@FXML
+	private ChoiceBox<String> choiceBox;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+		addChoice();
+		
+	}
+	
+	public void addChoice() {
+		choiceBox.getItems().add("nivel experto");
+		
+	}
+	
+	public void charge(ActionEvent  a) {
 		
 	}
 
